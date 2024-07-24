4ec01735573838e2740a74d8d38f5b738d903145
FlaringK <caelans@iinet.net.au>
2024-07-24 08:44:32 +0800

    Update Commission page

--- src/routes/commissions/+page.svelte
+++ src/routes/commissions/+page.svelte
@@ -3,20 +3,21 @@
 </script>
 
 <Window title="commisions.txt">
-  I do portfolio static website commissions! Specifically <b>personal websites for individuals</b>. Just like the site you’re looking at right now! I’ve been making website and messing around with Javascript and CSS for about 5 years now, you can see examples of my work in the <a href="/projects">Projects</a> tab. <br>
+  I do static website commissions! For personal portfolios and organisations; Just like the site you’re looking at right now! I’ve been making website and messing around with Javascript and CSS for about 5 years now, you can see examples of my work in the <a href="/projects">Projects</a> tab. <br>
   <br>
   This includes:
   <ul>
-    <li>Creating and coding a multipage website using either svelte or HTML/CSS/JS.</li>
-    <li>Helping find a free way to host your site online, using github or neocities.</li>
+    <li>Creating and coding a multipage website using either svelte, react, or more basic HTML/CSS/JS.</li>
+    <li>Helping find a free way to host your site online, using github or neocities. Or directing you to paid hosts.</li>
+    <li>Regular maintenance of the site (Updating content, creating new pages, etc)</li>
   </ul>
   This does <b>not</b> include:
   <ul>
     <li>Creating custom art or complex assets.</li>
-    <li>Connecting your site to any sort of backend (No stores, comments sections or email services, though I can recommend some services if you would like.)</li>
+    <li>Delevoping any sort of custom backend. (No stores, comments sections or email services, though I can recommend some much more user friendly services to connect to your site if you would like.)</li>
     <li>Paying for a custom url domain or host.</li>
   </ul>
-  Prices will fluctuate depending on the complexity of the site, but generally my rate is <b>$26 USD per hour</b>, and I will <b>charge for the first 3 hours up front</b> when starting a project. <br>
+  Prices will fluctuate depending on the complexity of the site, but generally my rate is <b>$48 USD per hour</b>, and I will <b>charge for the first 3 hours up front</b> when starting a project. <br>
   <br>
   You can contact me by emailing <a href="mailto:caelans@iinet.net.au">caelans@iinet.net.au</a>, direct message me on discord at <b>@flaringk</b>, or message me on any of the sites in the <a href="/contact">Contact Me</a> tab.
 </Window>
